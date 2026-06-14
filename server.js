918a5f961dedc27460214492a1424a60eca6aafa
Eslam Elgarhy <93857552+eslamelgarhy@users.noreply.github.com>
2026-06-14 20:23:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const db = mysql.createPool({
     host: process.env.MYSQLHOST || "mysql.railway.internal",
     user: process.env.MYSQLUSER || "root",
-    password: process.env.MYSQLPASSWORD || NDQctwbJNjhhdsOIGixDeauAfghVTYAI,
+    password: process.env.MYSQLPASSWORD || "NDQctwbJNjhhdsOIGixDeauAfghVTYAI",
     database: process.env.MYSQLDATABASE || "railway",
     port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
